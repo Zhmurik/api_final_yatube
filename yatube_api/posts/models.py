8f4f7b5036a40998ba37659b4da3b7f447a421cc
zhmurik <azhmurkova@gmail.com>
2021-08-06 17:22:41 -0700

    update models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,87 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=50)
+    description = models.TextField(default=None)
 
     def __str__(self):
-        return self.text
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to='posts/',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.text[:15]
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True)
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        default=None
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name='prevent_self_follow',
+                check=models.Q(user=models.F('following')),
+            ),
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_followers'
+            )
+        ]
+
+    def __str__(self):
+        return f'follower: {self.user}, following: {self.following}'
